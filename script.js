7c93c91df82afed2af52c18f0d1ddb2f2c54e6e7
samuelosafari <safarisamuel531@gmail.com>
2023-09-01 01:36:57 +0200

    update: made the card madals

--- script.js
+++ script.js
@@ -19,3 +19,43 @@ common = (x, y, z) => {
 
 common(open, close, "slide-in");
 common(close, open, "slide-out");
+
+//dealing with modals
+
+document.addEventListener("DOMContentLoaded", function () {
+  const learnMoreButtons = document.querySelectorAll(".card p");
+
+  function openModal(cardIndex) {
+    const modalContainer = document.getElementById(
+      `modal_container${cardIndex}`
+    );
+    const modal = modalContainer.querySelector(`#modal${cardIndex}`);
+    modalContainer.classList.remove("hide");
+    modal.classList.remove("hide");
+  }
+
+  function closeModal(cardIndex) {
+    const modalContainer = document.getElementById(
+      `modal_container${cardIndex}`
+    );
+    const modal = modalContainer.querySelector(`#modal${cardIndex}`);
+    modalContainer.classList.add("hide");
+    modal.classList.add("hide");
+  }
+
+  learnMoreButtons.forEach((button, index) => {
+    button.addEventListener("click", function () {
+      openModal(index + 1); // Card index is 1-based
+    });
+  });
+
+  // Close button event listeners for each modal
+  for (let i = 1; i <= 4; i++) {
+    const closeButton = document.getElementById(`close${i}`);
+    if (closeButton) {
+      closeButton.addEventListener("click", function () {
+        closeModal(i);
+      });
+    }
+  }
+});
