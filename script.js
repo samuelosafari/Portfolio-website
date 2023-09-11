d4617e649b907bbfa4a52df9d75a424adab505bf
samuelosafari <safarisamuel531@gmail.com>
2023-09-11 23:13:42 +0200

    updated: the scroll behaviour of a navbar

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 window.addEventListener("scroll", function () {
   var header = document.querySelector(".header");
-  if (window.scrollY > 90) {
+  if (window.scrollY > 80) {
     header.classList.add("scrolled");
   } else {
     header.classList.remove("scrolled");
